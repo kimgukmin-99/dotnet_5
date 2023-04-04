36aeb766167c50633647eeb6fbc383bee6ee3438
kimgukmin <rnrwk8303@naver.com>
2023-04-04 12:22:26 +0900

    초기 커밋.

---
+++ MusicPlayer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class Music
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public Music(string name, string author)
+    {
+        Name = name;
+        Author = author;
+    }
+
+    public void Music_Start(Music name)
+    {
+        string str = name.Name + " " + Author + " " + " 재생중입니다.";
+        Console.WriteLine(str);
+    }
+
+    public void Music_Stop()
+    {
+        Console.WriteLine("재생중인 음악 종료");
+    }
+}
+
+namespace ConsoleApp5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
